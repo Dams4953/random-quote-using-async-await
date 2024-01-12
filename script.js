56d6ff001ab701da360c66a437e1cffdd4912923
Damien <legat@CQJ>
2024-01-12 15:44:38 +0100

    fin

--- script.js
+++ script.js
@@ -1,27 +1,46 @@
 document.getElementById('bouton').addEventListener('click', async function () {
 
     try {
+        //auteurs
         let reponse = await fetch('https://thatsthespir.it/api');
-
         let data = await reponse.json();
+  
 
+        //âge des auteurs
+        let reponseAge = await fetch(`https://api.agify.io/?name=${data.author[0]}`);
+        let dataAge = await reponseAge.json();
+
+        //html
         let div = document.getElementById('div');
 
         div.innerHTML = '';
 
         let p1 = document.createElement('p');
         let p2 = document.createElement('p');
+        let p3 = document.createElement('p');
         let img = document.createElement('img');
 
-        p1.textContent = `${data.quote}`;
-        p2.textContent = `${data.author}`;
-        img.src = `${data.photo}`;
+
+        
+
+        p1.textContent = `${data.quote ?? 'citation inconnu'}`;
+        p2.textContent = `${data.author ?? 'auteur inconnu'}`;
+        p3.textContent = `${dataAge.age ?? 'âge inconnu'}`;
+
+        if (data.photo != '') {
+            img.src = data.photo 
+        } else {
+            img.src = `defaultimg.jpg`;
+        }
+        
 
 
         div.appendChild(p1)
         div.appendChild(p2)
+        div.appendChild(p3)
         div.appendChild(img)
 
+    //error
     } catch (error) {
         alert('erreur')
     }
